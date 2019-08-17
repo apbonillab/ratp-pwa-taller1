5cfc1490ba202257e86bf8cdf5ff7236cb4200b5
apbonillab <apbonillab@gmail.com>
2019-08-17 18:29:14 -0500

    F3 se adiciona cambios para service worker offline

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,23 @@
 'use strict';
 
-const CACHE_NAME = 'cache';
+const CACHE_DATA = 'cache';
 const SHELL_CACHE_NAME = 'shell-cache';
 
 const FILES_TO_CACHE = [
+  '/',
   '/index.html',
   '/scripts/app.js',
   '/images/ic_add_white_24px.svg',
   '/images/ic_refresh_white_24px.svg',
   '/styles/inline.css',
-  '/scripts/localforage.min.js'
+  '/scripts/localforage.min.js',
+  '/manifest.json'
 ];
 
 self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
   evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
+    caches.open( SHELL_CACHE_NAME).then((cache) => {
       console.log('[ServiceWorker] Pre-caching offline page');
       return cache.addAll(FILES_TO_CACHE);
     })
@@ -28,7 +30,7 @@ self.addEventListener('activate', (evt) => {
   evt.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME && key !== SHELL_CACHE_NAME) {
+        if (key !==  SHELL_CACHE_NAME && key !== CACHE_DATA) {
           console.log('[ServiceWorker] Removing old cache', key);
           return caches.delete(key);
         }
@@ -43,7 +45,7 @@ self.addEventListener('fetch', (evt) => {
 if (evt.request.url.includes('/schedules/')) {
   console.log('[Service Worker] Fetch (data)', evt.request.url);
   evt.respondWith(
-      caches.open(FILES_TO_CACHE).then((cache) => {
+      caches.open(CACHE_DATA).then((cache) => {
         return fetch(evt.request)
             .then((response) => {
               if (response.status === 200) {
@@ -57,7 +59,7 @@ if (evt.request.url.includes('/schedules/')) {
   return;
 }
 evt.respondWith(
-    caches.open(CACHE_NAME).then((cache) => {
+    caches.open( SHELL_CACHE_NAME).then((cache) => {
       return cache.match(evt.request)
           .then((response) => {
             return response || fetch(evt.request);
